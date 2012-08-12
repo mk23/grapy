7f2e31332a65211ebe9d6ee8b3f8b811eec7371d
Max Kalika <max@topsy.com>
2012-08-11 18:01:01 -0700

    Add simple ingestion server for simulating carbon.

---
+++ ingest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+
+import datetime
+from gevent.server import StreamServer
+
+def logger(msg, *args):
+    print '[%s] %s' % (datetime.datetime.now(), msg % args)
+
+def ingest(socket, remote):
+    logger('conn: %s:%s', *remote)
+
+    reader = socket.makefile()
+    while True:
+        line = reader.readline()
+        if not line:
+            logger('disc: %s:%s', *remote)
+            break
+
+        reader.flush()
+        logger('read: %r', line)
+
+
+if __name__ == '__main__':
+    logger('starting ingestion server on port 2004')
+
+    server = StreamServer(('0.0.0.0', 2004), ingest)
+    server.serve_forever()
