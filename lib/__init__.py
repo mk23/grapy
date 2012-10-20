cd9d1a7f36add7c0e6b1c4ddc7c97a11fdd0d24c
Max Kalika <max@topsy.com>
2012-10-20 10:22:56 -0700

    REORG REFACTOR: move parsing/lookup logic to stand-alone function to allow reuse from multiple pollers.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,14 @@
 import datetime
+import json
 import logging as log
 import os
 import pickle
+import re
 import socket
 import sys
 import time
 import traceback
+import urllib2
 
 class plugin:
     def __init__(self, conf):
@@ -51,6 +54,106 @@ class plugin:
             return '.'.join([quote(i) for i in label if i])
         else:
             return quote(label)
+
+    def lookup(self, data, label, parts, key=None, val=None):
+        if key is None:
+            key = []
+        if val is None:
+            val = {}
+
+        undo_one = undo_two = 0
+        try:
+            part, parts = parts[0], parts[1:]
+            if ':' in part:
+                indx, part = part.split(':')
+                if '|' in indx:
+                    indx, patt = (urllib2.unquote(s) for s in indx.split('|'))
+                    find = re.match('^%s$' % patt, data[indx])
+                    if find:
+                        key.extend(find.groups())
+                        undo_one += len(find.groups())
+                    else:
+                        raise ValueError('^%s$: no match in: %s' % (patt, data[indx]))
+                else:
+                    key.append(data[urllib2.unquote(indx)])
+                    undo_one += 1
+
+            if part == '**':
+                for atom in data:
+                    self.lookup(atom, label, parts, key, val)
+
+                for i in xrange(undo_one):
+                    undo_one -= 1
+                    key.pop()
+            else:
+                for atom in data:
+                    find = re.match('^%s$' % urllib2.unquote(part), atom)
+                    if find:
+                        key.extend(find.groups())
+                        undo_two += len(find.groups())
+
+                        if len(parts) > 0:
+                            self.lookup(data[atom], label, parts, key, val)
+                        else:
+                            val[label.format(name=self.escape(key) if key else atom)] = data[atom]
+
+                        for i in xrange(undo_two):
+                            undo_two -= 1
+                            key.pop()
+        finally:
+            for i in xrange(undo_one + undo_two):
+                key.pop()
+
+            return val
+
+    def parser(self, text):
+        data = getattr(self, 'parse_%s' % self.conf['parser'])(text)
+        for item in self.conf['items']:
+            label = item.get('label', '{name}')
+            parts = item['value'].lstrip('/').split('/')
+            for key, val in self.lookup(data, label, parts).items():
+                self.data[key] = val
+
+    def parse_json(self, data):
+        return json.loads(data.read())
+
+    def parse_text(self, data):
+        value = re.compile(self.conf['parser_conf']['value'])
+        parts = [re.compile(item) for item in self.conf['parser_conf'].get('parts', [])]
+
+        print data
+
+        items = {}
+        stack = [items]
+        for line in data:
+            match = value.match(line)
+            if match:
+                stack[-1][match.group('label')] = match.group('value')
+                continue
+
+            for i in xrange(len(stack) - 1, 0, -1):
+                if i == len(parts):
+                    stack.pop()
+                else:
+                    match = parts[i].match(line)
+                    if match:
+                        label = match.group(1)
+                        stack[-1][label] = {}
+                        stack.append(stack[-1][label])
+                        break
+                    else:
+                        stack.pop()
+            else:
+                if len(parts) < len(stack):
+                    continue
+
+                match = parts[len(stack) - 1].match(line)
+                if match:
+                    label = match.group(1)
+                    stack[-1][label] = {}
+                    stack.append(stack[-1][label])
+
+        return items
 
 
 def mk_sock(host, port=2004, cache={}):
--- lib/poller/http.py
+++ lib/poller/http.py
@@ -1,86 +1,13 @@
 import grapy
-import json
-import re
 import urllib2
 
 class http(grapy.plugin):
     def collect(self):
-        api_url = '{scheme}://{hostname}:{port}{path}'.format(**self.conf)
+        url = '{scheme}://{hostname}:{port}{path}'.format(**self.conf)
         if self.conf.get('auth_type') in ('basic', 'digest'):
-            pass_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-            pass_mgr.add_password(user=self.conf['auth_user'], passwd=self.conf['auth_pass'], uri=api_url, realm=None)
-            auth_hdl = getattr(urllib2, 'HTTP%sAuthHandler' % self.conf['auth_type'].title())(pass_mgr)
-            urllib2.install_opener(urllib2.build_opener(auth_hdl))
+            mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            mgr.add_password(user=self.conf['auth_user'], passwd=self.conf['auth_pass'], uri=url, realm=None)
+            hdl = getattr(urllib2, 'HTTP%sAuthHandler' % self.conf['auth_type'].title())(mgr)
+            urllib2.install_opener(urllib2.build_opener(hdl))
 
-        getattr(self, '%s_parser' % self.conf['parser'])(urllib2.urlopen(api_url).read())
-
-    def regex_parser(self, text):
-        for item in self.conf['items']:
-            find = re.search(item['regex'], text)
-            if find and len(find.groups()):
-                key = item.get('label', '').split('.') + [find.groupdict().get('label')]
-                val = find.groupdict().get('value', find.group(0))
-
-                self.data[self.escape(key)] = val
-
-
-
-    def json_parser(self, text):
-        data = json.loads(text)
-        for item in self.conf['items']:
-            label = item.get('label', '{name}')
-            parts = item['value'].lstrip('/').split('/')
-            for key, val in self.json_lookup(data, label, parts).items():
-                self.data[key] = val
-
-
-    def json_lookup(self, data, label, parts, key=None, val=None):
-        if key is None:
-            key = []
-        if val is None:
-            val = {}
-
-        undo_one = undo_two = 0
-        try:
-            part, parts = parts[0], parts[1:]
-            if ':' in part:
-                indx, part = part.split(':')
-                if '|' in indx:
-                    indx, patt = (urllib2.unquote(s) for s in indx.split('|'))
-                    find = re.match('^%s$' % patt, data[indx])
-                    if find:
-                        key.extend(find.groups())
-                        undo_one += len(find.groups())
-                    else:
-                        raise ValueError('^%s$: no match in: %s' % (patt, data[indx]))
-                else:
-                    key.append(data[urllib2.unquote(indx)])
-                    undo_one += 1
-
-            if part == '**':
-                for atom in data:
-                    self.json_lookup(atom, label, parts, key, val)
-
-                for i in xrange(undo_one):
-                    undo_one -= 1
-                    key.pop()
-            else:
-                for atom in data:
-                    find = re.match('^%s$' % urllib2.unquote(part), atom)
-                    if find:
-                        key.extend(find.groups())
-                        undo_two += len(find.groups())
-
-                        if len(parts) > 0:
-                            self.json_lookup(data[atom], label, parts, key, val)
-                        else:
-                            val[label.format(name=self.escape(key) if key else atom)] = data[atom]
-
-                        for i in xrange(undo_two):
-                            undo_two -= 1
-                            key.pop()
-        finally:
-            for i in xrange(undo_one + undo_two):
-                key.pop()
-
-            return val
+        self.parser(urllib2.urlopen(url))
