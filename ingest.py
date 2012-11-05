7b144d60bd020a1743fd9216176e928a77491913
Max Kalika <max@topsy.com>
2012-11-05 03:07:09 -0800

    Support text as well as pickle protocol in the ingester.

--- ingest.py
+++ ingest.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python2.7
 
-import datetime, sys
+import datetime
+import pickle
+import struct
+import sys
+
 from gevent.server import StreamServer
 
 def logger(msg, *args):
     print '[%s] %s' % (datetime.datetime.now(), msg % args)
 
-def ingest(socket, remote):
+def ingest_text(socket, remote):
     logger('conn: %s:%s', *remote)
 
     reader = socket.makefile()
@@ -19,11 +23,33 @@ def ingest(socket, remote):
         reader.flush()
         logger('read: %r', line)
 
+def ingest_pickle(socket, remote):
+    logger('conn: %s:%s', *remote)
+
+    while True:
+        head = struct.unpack('!L', socket.recv(4))[0]
+        data = pickle.loads(socket.recv(head))
+
+        logger('read: %d len payload', head)
+        for item in data:
+            logger('data: %s %s %d', item[0], item[1][1], item[1][0])
+
+        break
+
+
 
 if __name__ == '__main__':
-    logger('starting ingestion server on port 2004')
+    import argparse
 
-    server = StreamServer(('0.0.0.0', 2004), ingest)
+    parser = argparse.ArgumentParser(description='carbon server simulator')
+    parser.add_argument('-p', '--port', default=2003, type=int,
+                        help='server port')
+    parser.add_argument('-t', '--type', default='text', choices=['text', 'pickle'],
+                        help='protocol type')
+    args = parser.parse_args()
+
+    logger('starting ingestion server on port 2004')
+    server = StreamServer(('0.0.0.0', args.port), getattr(sys.modules[__name__], 'ingest_%s' % args.type))
     try:
         server.serve_forever()
     except KeyboardInterrupt:
